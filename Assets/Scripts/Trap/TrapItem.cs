cb81b5562f451f2248249a7ae37c0604f2b75fbe
GearIncQhuy <tqhuy9701@gmail.com>
2024-02-04 02:45:46 +0700

    HuyTQ_Create TrapItem

---
+++ Assets/Scripts/Trap/TrapItem.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TrapItem : Trap {
+
+  public TrapItemState trapItemState;
+  public enum TrapItemState {
+    noHide,
+    Hide
+  }
+
+  [SerializeField] private SpriteRenderer spriteRenderer;
+  [SerializeField] private int countHealth;
+  [SerializeField] private GameObject item;
+  [SerializeField] private Transform pointToSpawn;
+
+  private bool itemSpawned = false;
+  private bool isCollided = false;
+  private bool playerCollided = false;
+
+  private void OnValidate() {
+    if (spriteRenderer == null) spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+  private void Update() {
+    base.Update();
+    RaycastCheck();
+
+    switch (trapItemState) {
+      case TrapItemState.noHide:
+        spriteRenderer.enabled = true;
+        break;
+      case TrapItemState.Hide:
+        spriteRenderer.enabled = isCollided;
+        break;
+    }
+  }
+
+  protected override void RaycastCheck() {
+    int countspawn = 1;
+    RaycastHit2D hitup = Physics2D.BoxCast(transform.position, new Vector2(raycastWidth, raycastHeight), 0f, Vector2.down, distance, playerLayer);
+    Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
+    if (hitup.collider != null) {
+      if (trapItemState == TrapItemState.Hide && !isCollided) {
+        isCollided = true;
+        return;
+      }
+      if (!playerCollided) {
+        countHealth--;
+        playerCollided = true;
+      }
+      if(countHealth <= 0 && !itemSpawned) {
+        countHealth = 0;
+        for (int i = 0; i < countspawn; i++) {
+          GameObject spawnedItem = Instantiate(item, transform.position, Quaternion.identity);
+          spawnedItem.transform.DOMove(pointToSpawn.position, 1f);
+        }
+        itemSpawned = true;
+      }
+    } else {
+      playerCollided = false;
+    }
+  }
+}
