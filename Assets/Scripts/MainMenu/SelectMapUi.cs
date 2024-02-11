88cc62dfec262a8a9b89464a21398460576425f5
GearIncQhuy <tqhuy9701@gmail.com>
2024-02-11 13:19:25 +0700

    HuyTQ_Update check unlock map

--- Assets/Scripts/MainMenu/SelectMapUi.cs
+++ Assets/Scripts/MainMenu/SelectMapUi.cs
@@ -5,7 +5,9 @@ public class SelectMapUi : MonoBehaviour {
   [SerializeField] private SelectMapData selectMapData;
 
   public void SelectMapIndex(int mapIndex) {
-    selectMapData.currentMapIndex = mapIndex;
-    SceneManager.LoadScene(Constants.Scene_StartGame);
+    if (selectMapData.MapData[mapIndex].isUnlocked) {
+      selectMapData.currentMapIndex = mapIndex;
+      SceneManager.LoadScene(Constants.Scene_StartGame);
+    }
   }
 }
