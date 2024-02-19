6c62f0bceff7eb8369e1506dd8119c5e441c1b27
GearIncQhuy <tqhuy9701@gmail.com>
2024-02-19 16:15:43 +0700

    HuyTQ_Import Assets and update map

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,12 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-    public Transform target;
-    public float smoothSpeed = 0.125f;
+public class CameraFollow : MonoBehaviour {
+  public Transform target;
+  public float smoothSpeed = 0.125f;
 
-    private void Update() {
-        transform.position = new Vector3(target.position.x, 4, transform.position.z);
-    }
+  private void Update() {
+    Vector3 desiredPosition = new Vector3(target.position.x, 4, transform.position.z);
+    Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+    transform.position = smoothedPosition;
+  }
 }
--- Assets/Scripts/Map/CreateMap.cs
+++ Assets/Scripts/Map/CreateMap.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateMap : MonoBehaviour {
   [SerializeField] private SelectMapData selectMapData;
-
+  [SerializeField] private Text textLevelmap;
   private void Awake() {
     Instantiate(selectMapData.MapData[selectMapData.currentMapIndex].prefabs, new Vector3(0, 0, 0), Quaternion.identity);
+  }
+  
+  private void Start() {
+    textLevelmap.text = "Level " + selectMapData.MapData[selectMapData.currentMapIndex + 1].currentLevel;
   }
 }
